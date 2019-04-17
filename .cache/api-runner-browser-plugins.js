1acbae172efafc0aede2dac19691f9e4b7bc0e34
chpark <beakya884@gmail.com>
2019-04-17 21:43:12 +0900

    update gatsby

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,22 @@
+module.exports = [{
+      plugin: require('/Users/chpark/projects/baekya884.github.io/node_modules/gatsby-remark-images/gatsby-browser.js'),
+      options: {"plugins":[],"maxWidth":960},
+    },{
+      plugin: require('/Users/chpark/projects/baekya884.github.io/node_modules/gatsby-remark-autolink-headers/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/chpark/projects/baekya884.github.io/node_modules/gatsby-plugin-netlify-cms/gatsby-browser.js'),
+      options: {"plugins":[],"modulePath":"/Users/chpark/projects/baekya884.github.io/src/cms/cms.js"},
+    },{
+      plugin: require('/Users/chpark/projects/baekya884.github.io/node_modules/gatsby-plugin-google-gtag/gatsby-browser.js'),
+      options: {"plugins":[],"trackingIds":["UA-73379983-2"],"pluginConfig":{"head":true}},
+    },{
+      plugin: require('/Users/chpark/projects/baekya884.github.io/node_modules/gatsby-plugin-offline/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/chpark/projects/baekya884.github.io/node_modules/gatsby-plugin-catch-links/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/chpark/projects/baekya884.github.io/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
